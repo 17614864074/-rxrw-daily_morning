e5c07d96e87ce06e057e9c2e448a7249842591ac
17614864074 <95196619+17614864074@users.noreply.github.com>
2022-09-23 10:41:56 +0800

    url

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   print(url)
-  return url
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])

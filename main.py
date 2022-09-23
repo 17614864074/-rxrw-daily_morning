b90dd9c8cfce1088473817e82262f16aafbc4cc2
17614864074 <95196619+17614864074@users.noreply.github.com>
2022-09-23 10:33:17 +0800

    查看城市和请求路径

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   console.log(city)
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  console.log(url)
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
